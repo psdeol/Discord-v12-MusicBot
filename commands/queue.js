e1d3f577aa7125328a570c5804a6599748e9806b
singhp4 <singhp4@wwu.edu>
2021-09-06 14:57:32 -0700

    slight changes to queue commands

--- commands/queue.js
+++ commands/queue.js
@@ -18,8 +18,8 @@ module.exports = {
         let index = 1;
         let string = "";
 
-        if(server_queue.songs[0]) string += `__**Currently playing**__\n ${server_queue.songs[0].title}\n\n`;
-        if(server_queue.songs[1]) string += `__**Rest of queue**__\n ${server_queue.songs.slice(1, 10).map(x => `**${index++})** ${x.title}`).join("\n")}`;
+        if(server_queue.songs[0]) string += `__**Currently Playing**__\n ${server_queue.songs[0].title}\n\n`;
+        if(server_queue.songs[1]) string += `__**Next 10 Songs in Queue**__\n ${server_queue.songs.slice(1, 10).map(x => `**${index++})** ${x.title}`).join("\n")}`;
 
         let embed = new Discord.MessageEmbed()
         .setAuthor(`Current Queue for ${message.guild.name}`, message.guild.iconURL)
